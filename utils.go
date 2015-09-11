a45cd094e376d30448313481effacffab69fb875
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-09-11 09:41:49 +0900

    go fmt

--- utils.go
+++ utils.go
@@ -15,12 +15,12 @@ import (
 )
 
 func stringInSlice(str string, list []string) bool {
- 	for _, v := range list {
- 		if v == str {
- 			return true
- 		}
- 	}
- 	return false
+	for _, v := range list {
+		if v == str {
+			return true
+		}
+	}
+	return false
 }
 
 func checkType(a string, b string) {
